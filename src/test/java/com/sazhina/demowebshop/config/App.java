86e9f921ea9a2a5ac41d91cbdb271d386169e186
Elena <Lena-Sazh@users.noreply.github.com>
2021-11-28 13:36:20 +0300

    Delete some classes

--- src/test/java/com/sazhina/demowebshop/config/App.java
+++
@@ -1,7 +0,0 @@
-package com.sazhina.demowebshop.config;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
